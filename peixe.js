8f62a8875e2fd2c0ba5a0db448ead06c7d1cb489
RodrigoLeee <158863726+RodrigoLeee@users.noreply.github.com>
2024-02-07 15:53:43 -0300

    tubarao e logo

--- peixe.js
+++ peixe.js
@@ -13,11 +13,27 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    //carrega fundo
     this.load.image('mar', 'assets/bg_azul-escuro.png');
+
+    //carrega o logo
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
+
+    //carrega o tubarao
+    this.load.image('tubarao', 'assets/peixes/tubarao.png');
 }
+
 function create() {
+    //adiciona fundo
     this.add.image(400, 300, 'mar');
+
+    //adiciona o logo
+    this.add.image(400, 525, 'logo').setScale(0.5);
+
+    //adiciona o tubarao
+    this.add.image(400, 300, 'tubarao').setOrigin(0.5, 0.5).setFlip(true, false);
 }
+
 function update() {
     
 }
